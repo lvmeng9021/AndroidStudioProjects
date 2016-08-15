3c787afb58257e7dddb712a4703c624f618c5e81
lvmeng1126 <lvmeng1126@spreadst.com>
2016-08-15 19:56:02 +0800

    First commit. Build actionbar demos projects.

---
+++ ActionBarDemos/EffectiveNavigation/src/main/java/com/demos/lvmeng/actionbardemos/EffectiveNavigation.java
@@ -0,0 +1,13 @@
+package com.demos.lvmeng.actionbardemos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EffectiveNavigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_effective_navigation);
+    }
+}
