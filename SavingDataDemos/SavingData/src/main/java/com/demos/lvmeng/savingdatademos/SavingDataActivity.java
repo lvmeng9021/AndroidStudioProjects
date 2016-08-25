9f11c243e6e23d7f60d3cc0595bf924c9cc9517a
lvmeng9021 <lvmeng9021@gmail.com>
2016-08-26 00:08:45 +0800

    Owner:lvmeng9021
    Date: 08/26/2016
    
    Modify:
    Add SavingDataDemos project.

---
+++ SavingDataDemos/SavingData/src/main/java/com/demos/lvmeng/savingdatademos/SavingDataActivity.java
@@ -0,0 +1,13 @@
+package com.demos.lvmeng.savingdatademos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavingDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saving_data);
+    }
+}
